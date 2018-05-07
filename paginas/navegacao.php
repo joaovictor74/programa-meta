0ea66c53ac0462dc2beabcee6316a01262ab7d57
Joao <joao.victor74@hotmail.com>
2018-05-06 23:54:32 -0300

    Melhorias na nav #Vagner-Foda

--- paginas/navegacao.php
+++ paginas/navegacao.php
@@ -1,14 +1,55 @@
-<nav class="navbar navbar-expand navbar-dark fixed-top " style="background-color:#162930;">
-    <!--Brand-->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top " style="background-color:#162930;">
+    <div class="container-fluid">
+        <!--Brand-->
+        <a class="navbar-brand" href="presidio-index.php">PRESIDIO TAL</a>
+
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggler collapsed" data-toggle="collapse" data-target="#barra-navegacao">
+                <span class="sr-only">Alternar Menu</span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <!--Inicio-->
+        <div class="collapse navbar-collapse" id="barra-navegacao">
+            <ul class="nav navbar">
+                <li class="nav-item">
+                    <a class="nav-link" href="presidio-index.php" style="">Inicio</a>
+                </li>
+                <!--DropDown Cadastro-->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Cadastro</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="agente_form.php">Cadastro Agente</a>
+                        <a class="dropdown-item" href="detento_form.php">Cadastro Reeducando</a>
+                    </div>
+                </li>
+                <!--DropDown Tabelas-->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Tabelas</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="agente_tabela.php">Tabela de Agentes</a>
+                        <a class="dropdown-item" href="#">Tabela de Reeducandos</a>
+                        <a class="dropdown-item" href="#">Tabela das Funções</a>
+                        <a class="dropdown-item" href="#">Tabala dos Locais</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!--
+    <nav class="navbar navbar-expand navbar-dark fixed-top " style="background-color:#162930;">
+        Brand
     <div class="navbar-header">
         <a class="navbar-brand" href="presidio-index.php">PRESIDIO TAL</a>
     </div>
-    <!--Inicio-->
+    Inicio
     <ul class="nav navbar ">
         <li class="nav-item">
             <a class="nav-link" href="presidio-index.php" style="">Inicio</a>
         </li>
-        <!--DropDown Cadastro-->
+        DropDown Cadastro
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Cadastro</a>
             <div class="dropdown-menu">
@@ -16,7 +57,7 @@
                 <a class="dropdown-item" href="detento_form.php">Cadastro Reeducando</a>
             </div>
         </li>
-        <!--DropDown Tabelas-->
+        DropDown Tabelas
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Tabelas</a>
             <div class="dropdown-menu">
@@ -27,6 +68,7 @@
             </div>
         </li>
     </ul>
-</nav>
+    </nav>
+-->
 
 
